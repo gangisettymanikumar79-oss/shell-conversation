908f5ead2ab9ae78a77ebce5b07d83b6c6ccbe3a
.email.com <gangisettymani98@gmail.com>
2026-07-01 18:05:00 +0530

    i want create new file but if you don't open the file

--- shell-variable.sh
+++ shell-variable.sh
@@ -7,4 +7,3 @@ echo "Pakistan": "If you touch India, the war will start immediately."
 echo "India": "This is the last time I will release this bomb."
 
 echo "Pakistan": "I've already seen your bomb, India."
-
