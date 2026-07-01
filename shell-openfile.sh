493774ff1dc10612e441681ff5257efeeb781f85
.email.com <gangisettymani98@gmail.com>
2026-07-01 18:45:39 +0530

    i have update file

---
+++ shell-openfile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+person1=$trump
+person2=$iran
+echo "$trump": "Hello, $iran."
+
+echo "$iran": "If you attack $trump, war will begin immediately."
+
+echo "$trump": "This is my final warning. I will use this bomb."
+
+echo "$iran": "We've already seen your bomb, $trump. We're ready."
