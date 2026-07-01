042d325eca2f07f2d8f5587f6c02bb377c4c4a9e
.email.com <gangisettymani98@gmail.com>
2026-07-01 19:17:14 +0530

    i have update file

--- shell-password.sh
+++ shell-password.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-timestamp-start=$(date)
+timestamp_start=$(date +%s)
 
-echo "$timestamp-start"
+sleep 10
+
+timestamp_end=$(date +%s)
+
+echo "Start time: $timestamp_start"
+echo "End time: $timestamp_end"
+
+duration=$((timestamp_end - timestamp_start))
+
+echo "Script execution time: ${duration} seconds"
+
+
+
+
