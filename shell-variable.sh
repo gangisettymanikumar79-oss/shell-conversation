440f7326fb0138f06894cf2fe1636cd7e45e2997
.email.com <gangisettymani98@gmail.com>
2026-07-01 17:55:01 +0530

    i want create another file

---
+++ shell-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "India": "Hello, Pakistan."
+
+echo "Pakistan": "If you touch India, the war will start immediately."
+
+echo "India": "This is the last time I will release this bomb."
+
+echo "Pakistan": "I've already seen your bomb, India."
+
