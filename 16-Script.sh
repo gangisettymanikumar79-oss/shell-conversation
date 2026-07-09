09692aa80f8a76e0c303440a234ee1b06237c2b7
.email.com <gangisettymani98@gmail.com>
2026-07-09 16:16:15 +0530

    if i want create updateing file

--- 16-Script.sh
+++ 16-Script.sh
@@ -1,11 +1,13 @@
 
 #!/bin/bash
 
-Country=india 
-echo "Country :$Country"
-echo "pid of script -1:$$"
-#######sh  17-script-2.sh
+Country=india
+echo "Country: $Country"
+echo "PID of script-1: $$"
+
 source ./17-script-2.sh
+
+
 
 
 
