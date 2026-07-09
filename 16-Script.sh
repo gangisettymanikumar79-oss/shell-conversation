9fffd4b7bf756715923e7a2ae35b05e75f44b297
.email.com <gangisettymani98@gmail.com>
2026-07-09 16:06:51 +0530

    if i want create updateing file

---
+++ 16-Script.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+
+Country=india 
+echo "Country :$Country"
+echo "pid of script -1:$$"
+sh  17-script-2.sh
---
+++ shell-start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+timestamp_start=$(date +%s)
+
+sleep 10
+
+timestamp_end=$(date +%s)
+
+echo "Start time: $timestamp_start"
+echo "End time: $timestamp_end"
+
+duration=$((timestamp_end - timestamp_start))
+
+echo "Script execution time: ${duration} seconds"
